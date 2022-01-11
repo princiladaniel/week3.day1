ddb6c1ea67df9ada71ec0cab32f85b2859d296e4
princiladaniel <93445762+princiladaniel@users.noreply.github.com>
2022-01-11 23:15:35 +0530

    Add files via upload

---
+++ system/Computer.java
@@ -0,0 +1,29 @@
+package org.system;
+
+public class Computer {
+	String a="Dell";
+	   String b="Hp";
+public void computerModel() {
+	System.out.println(a);
+	System.out.println(b);
+}
+	public static void main(String[] args) {
+		
+		System.out.println("ComputerModel");
+	
+		
+		
+}
+}
+
+/*Assignment1:
+===========
+     Package   :org.system
+     Class        :Computer
+     Methods   :computerModel()
+
+     Class        :Desktop
+     Methods   :desktopSize()
+
+Description:
+create above 2 class and call all your class methods into the Desktop using single inheritance.*/
---
+++ system/Desktop.java
@@ -0,0 +1,13 @@
+package org.system;
+public class Desktop extends Computer {
+public void desktopSize() {
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Desktop model=new Desktop();
+		model.computerModel();
+
+	}
+
+}
