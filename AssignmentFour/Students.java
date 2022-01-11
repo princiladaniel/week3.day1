db8026d3f166290948423e232b6657c68cafad1f
princiladaniel <93445762+princiladaniel@users.noreply.github.com>
2022-01-11 23:38:23 +0530

    Add files via upload

---
+++ AssignmentFour/Students.java
@@ -0,0 +1,29 @@
+package AssignmentFour;
+
+
+public class Students {
+
+	public void getStudentInfo(int id) {
+		
+	}
+	public void getStudentInfo(int id,String name) {
+		
+	}
+	public void getStudentInfo(String email,int phoneNumber ) { 
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/* Assignment 4:
+==============
+
+      Class: Students
+      Methods: getStudentInfo()
+
+Description: 
+Create multiple getStudentInfo() method by passing id argument alone, by id & name, by email & phoneNumber */
