9623ed4c845b95592c793619d2dbc3ddba6b51d0
princiladaniel <93445762+princiladaniel@users.noreply.github.com>
2022-01-11 23:10:57 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,30 @@
+package Week3.day1;
+
+public class BankInfo {
+String save="saving",depo="amount";
+
+public void saving() {
+	System.out.println(save);
+}
+public void fixed() {
+	System.out.println("fixed");
+}
+public void deposit() {
+	System.out.println("deposit");
+}
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+System.out.println("Bankinfromation");
+	}
+}
+/*Assignment3:
+============
+Class     :BankInfo
+Methods :saving(),fixed(),deposit()
+
+ Class     :AxisBank
+ Methods :deposit()
+
+Description:
+You have to override the method deposit in AxisBank.*/
